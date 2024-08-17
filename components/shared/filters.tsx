348e3c22d0a520234b57ecf867935a0c8f5b3cff
alice29shevchuk <Sevc_sd76@student.itstep.org>
2024-08-17 14:11:06 +0300

    added queryString and searchParams, selected ingredients

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,113 +1,131 @@
-import React from 'react'
-import { cn } from '@/lib/utils';
-import { Title } from './title';
-import { FilterCheckbox } from './filter-checkbox';
-import { Input } from '../ui';
-import { RangeSlider } from './range-slider';
-import { CheckboxFiltersGroup } from './checkbox-filters-group';
-interface Props{
-    className?:string;
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Title } from "./title";
+import { FilterCheckbox } from "./filter-checkbox";
+import { Input } from "../ui";
+import { RangeSlider } from "./range-slider";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
+import { useFilterIngredients } from "@/hooks/useFilterIngredients";
+import { useSet } from "react-use";
+import qs from "qs";
+import { useRouter, useSearchParams } from "next/navigation";
+interface Props {
+  className?: string;
 }
+interface PriceProps {
+  priceFrom?: number;
+  priceTo?: number;
+}
+interface QueryFilters extends PriceProps{
+  pizzaTypes: string;
+  sizes: string;
+  ingredients: string;
+}
+export const Filters: React.FC<Props> = ({ className }) => {
+  const searchParams = useSearchParams() as unknown as Map<keyof QueryFilters,string>;
+  const { ingredients, loading, onAddId, selectedIngredients } = useFilterIngredients();
+  const router = useRouter();
+  const[sizes,{toggle: toggleSizes}] = useSet<string>(new Set(searchParams.get('sizes') ? searchParams.get('sizes')?.split(','):[]));
+  const[pizzaTypes,{toggle: togglePizzaTypes}] = useSet<string>(new Set(searchParams.get('pizzaTypes') ? searchParams.get('pizzaTypes')?.split(','):[]));
 
-export const Filters: React.FC<Props> = ({className})=> {
+  const [prices, setPrice] = React.useState<PriceProps>({
+    priceFrom: Number(searchParams.get('priceFrom')) || undefined,
+    priceTo: Number(searchParams.get('priceTo')) || undefined,
+  });
+  const items = ingredients.map((item) => ({
+    value: String(item.id),
+    text: item.name,
+  }));
+  const updatePrice = (name: keyof PriceProps, value: number) => {
+    setPrice({ ...prices, [name]: value });
+  };
+
+  React.useEffect(()=>{
+    const filters={
+      ...prices,
+      pizzaTypes: Array.from(pizzaTypes),
+      sizes: Array.from(sizes),
+      selectedIngredients: Array.from(selectedIngredients),
+    }
+    const query=  qs.stringify(filters,{
+      arrayFormat: 'comma',
+    });
+    router.push(`?${query}`,{
+      scroll:false,
+    });
+  },[prices,pizzaTypes,sizes,ingredients,selectedIngredients,router])
   return (
     <div className={className}>
-       <Title text="Фильтрация" size="sm" className='mb-5 font-bold'></Title>
-       <div className='flex flex-col gap-4'>
-            <FilterCheckbox text="Можно собирать" value="1"></FilterCheckbox>
-            <FilterCheckbox text="Новинки" value="2"></FilterCheckbox>
-       </div>
+      <Title text="Фильтрация" size="sm" className="mb-5 font-bold"></Title>
 
-       <div className='mt-5 border-y border-y-neutral-100 py-6 pb-7'>
-        <p className='font-bold mb-3'>Цены от и до: </p>
-        <div className='flex gap-3 mb-5'>
-            <Input type='number' placeholder='0' min={0} max={800} defaultValue={0}></Input>
-            <Input type='number' min={100} max={800} placeholder='800'></Input>
+
+      <CheckboxFiltersGroup
+        title="Тип теста"
+        name="pizzaTypes"
+        className="mt-5"
+        onClickCheckbox={togglePizzaTypes}
+        selected={pizzaTypes}
+        items={[
+          {text:'Тонкое', value: '1'},
+          {text:'Традиционное', value: '2'},
+        ]}
+      ></CheckboxFiltersGroup>
+
+
+      <CheckboxFiltersGroup
+        title="Размеры"
+        name="sizes"
+        className="mt-5"
+        onClickCheckbox={toggleSizes}
+        selected={sizes}
+        items={[
+          {text:'20 см', value: '20'},
+          {text:'30 см', value: '30'},
+          {text:'40 см', value: '40'}
+        ]}
+      ></CheckboxFiltersGroup>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Цены от и до: </p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={800}
+            value={String(prices.priceFrom)}
+            onChange={(e)=>{updatePrice('priceFrom', Number(e.target.value))}}
+          ></Input>
+          <Input
+            type="number"
+            min={100}
+            max={800}
+            placeholder="800"
+            value={String(prices.priceTo)}
+            onChange={(e)=>{updatePrice('priceTo', Number(e.target.value))}}
+          ></Input>
         </div>
 
-        <RangeSlider min={0} max={1000} step={10} value={[0,1000]}></RangeSlider>
-       </div>
-       <CheckboxFiltersGroup 
-          title='Ингредиенты' 
-          className='mt-5' 
-          limit={6} 
-          defaultItems={[
-            {
-              text:'Сырный соус',
-              value:'1',
-            },
-            {
-              text:'Моцарелла',
-              value:'2',
-            },
-            {
-              text:'Чеснок',
-              value:'3',
-            },
-            {
-              text:'Соленые огурчики',
-              value:'4',
-            },
-            {
-              text:'Красный лук',
-              value:'5',
-            },
-            {
-              text:'Томаты',
-              value:'6',
-            },
-          ]}
-          items={[
-            {
-              text:'Сырный соус',
-              value:'1',
-            },
-            {
-              text:'Моцарелла',
-              value:'2',
-            },
-            {
-              text:'Чеснок',
-              value:'3',
-            },
-            {
-              text:'Соленые огурчики',
-              value:'4',
-            },
-            {
-              text:'Красный лук',
-              value:'5',
-            },
-            {
-              text:'Томаты',
-              value:'6',
-            },
-            {
-              text:'Сырный соус',
-              value:'1',
-            },
-            {
-              text:'Моцарелла',
-              value:'2',
-            },
-            {
-              text:'Чеснок',
-              value:'3',
-            },
-            {
-              text:'Соленые огурчики',
-              value:'4',
-            },
-            {
-              text:'Красный лук',
-              value:'5',
-            },
-            {
-              text:'Томаты',
-              value:'6',
-            },
-          ]}
-          ></CheckboxFiltersGroup>
+        <RangeSlider
+          min={0}
+          max={800}
+          step={10}
+          value={[prices.priceFrom || 0, prices.priceTo || 800]}
+          onValueChange={([priceFrom, priceTo]) => setPrice({priceFrom, priceTo})}
+        ></RangeSlider>
+      </div>
+      <CheckboxFiltersGroup
+        title="Ингредиенты"
+        name="ingredients"
+        className="mt-5"
+        limit={6}
+        defaultItems={items.slice(0, 6)}
+        items={items}
+        loading={loading}
+        onClickCheckbox={onAddId}
+        selected={selectedIngredients}
+      ></CheckboxFiltersGroup>
     </div>
-  )
-}
+  );
+};
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from './container';
 import Image from 'next/image';
 import { ShoppingCart, User,ArrowRight } from 'lucide-react';
 import { Button } from '../ui';
+import Link from 'next/link';
+import { SearchInput } from './search-input';
 interface Props{
     className?:string;
 }
@@ -12,12 +14,18 @@ export const Header: React.FC<Props> = ({className})=> {
   return (
     <header className={cn('border border-b',className)}>
         <Container className='flex items-center justify-between py-8'>
-            <div className='flex items-center gap-4'>
-                <Image className='w-[55px] h-[55px]' src="https://pizzaday.eatery.club/storage/pizzaday/restaurant/logo/1185/ac39f6bdf5c716aec904fe503002094b.png" alt='Logo' width={35} height={35}></Image>
-                <div>
-                    <h1 className='text-2xl uppercase font-black'>Pizza</h1>
-                    <p className='text-sm text-gray-400 leading-3'>вкусней уже некуда</p>
+           <Link href='/'>
+                <div className='flex items-center gap-4'>
+                    <Image className='w-[55px] h-[55px]' src="https://pizzaday.eatery.club/storage/pizzaday/restaurant/logo/1185/ac39f6bdf5c716aec904fe503002094b.png" alt='Logo' width={35} height={35}></Image>
+                    <div>
+                        <h1 className='text-2xl uppercase font-black'>Pizza</h1>
+                        <p className='text-sm text-gray-400 leading-3'>вкусней уже некуда</p>
+                    </div>
                 </div>
+            </Link>
+            
+            <div className='mx-10 flex-1'>
+                <SearchInput></SearchInput>
             </div>
 
             <div className='flex items-center gap-3'>
